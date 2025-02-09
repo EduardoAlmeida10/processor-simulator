2a2d2bb8605f07eec5b1134037b90f3ebfc31c98
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-02-09 15:24:04 -0300

    inicialização das variáveis

--- processor-simulator-c/main.c
+++ processor-simulator-c/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
+
+// criação e inicialização das variaveis
+typedef struct CPU{
+    uint16_t R[8];
+    uint16_t PC;
+    uint32_t SP;
+    uint16_t IR;
+    uint8_t C, Ov, Z, S;
+}CPU;
+
+CPU cpu = { {0}, 0x0000, 0x0000, 0x82000000, 0, 0, 0, 0 };
 
 int main(){
-    printf("Hello word!");
+    
 }
