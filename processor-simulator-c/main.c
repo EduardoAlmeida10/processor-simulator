982a371918d8a562f1766d1276df0522cb9dbd83
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-02-10 11:47:28 -0300

    funcao do estado da cpu

--- processor-simulator-c/main.c
+++ processor-simulator-c/main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define MEM_SIZE 65536
+#define MEM_SIZE 255
 
 // criação e inicialização das variaveis
 typedef struct CPU{
@@ -10,10 +10,10 @@ typedef struct CPU{
     uint16_t PC;
     uint32_t SP;
     uint16_t IR;
-    uint8_t C, Ov, Z, S;
+    int C, Ov, Z, S;
 }CPU;
 
-CPU cpu = { {0}, 0x0000, 0x0000, 0x82000000, 0, 0, 0, 0 };
+CPU cpu = { {0}, 0x0000, 0x82000000, 0x0000, 0, 0, 0, 0 };
 uint16_t memory[MEM_SIZE];
 
 // leitura do arquivo;
@@ -42,6 +42,21 @@ void ReadFile(const char *nameFile){
     fclose(file);
 }
 
+// funcao para mostrar os estados dos registradores, do sp e pc, alem das flags
+void State(){
+    printf("REGISTRADORES:\n");
+    for(int i = 0; i < 8; i++){
+        printf("R%d: 0x%04X\n", i, cpu.R[i]);
+    }
+    printf("PC: 0x%04X SP: 0x%08X\n", cpu.PC, cpu.SP);
+    printf("FLAGS:\n");
+    printf("C: %d\nOv: %d\nZ: %d\nS: %d\n", cpu.C, cpu.Ov, cpu.Z, cpu.S);
+}
+
 int main(){
     ReadFile("../instructions.txt");
+
+    State();
+
+    return 0;
 }
